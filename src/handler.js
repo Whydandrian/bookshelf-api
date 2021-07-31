184786b5462a60bb8ebcda7359893d50e630e471
whydandrian <whydandrian@gmail.com>
2021-07-31 21:02:48 +0700

    add condition if !name & if readPafe is > from pageCount

--- src/handler.js
+++ src/handler.js
@@ -119,6 +119,25 @@ const editBookByIdHandler = (request, h) => {
   const { name, year, author, summary,
     publisher, pageCount, readPage,
     reading } = request.payload;
+
+  if (!name || name === undefined) {
+    const response = h.response({
+      "status": "fail",
+      "message": "Gagal memperbarui buku. Mohon isi nama buku"
+    });
+    response.code(400);
+    return response;
+  }
+  
+  if (readPage > pageCount) {
+    const response = h.response({
+      "status": "fail",
+      "message": "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+    });
+    response.code(400);
+    return response;
+  }
+
   const updatedAt = new Date().toISOString();
   const index = books.findIndex((book) => book.id === id);
 
@@ -141,7 +160,7 @@ const editBookByIdHandler = (request, h) => {
 
   const response = h.request({
     status: 'fail',
-    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
   });
   response.code(404);
   return response;
